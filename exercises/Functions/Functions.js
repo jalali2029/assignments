2f7ca87334594bdc2505b13a66ba7bb0a998d01f
jalali2029 <learn@vschool.io>
2018-03-02 19:29:52 -0700

    Colors from Grid

---
+++ exercises/Functions/Functions.js
@@ -0,0 +1,36 @@
+// var Add = function (a,b){
+//     return a+b
+// }
+// Add (5,8)
+
+// var Lrg = function (a,b,c){
+//     console.log(Math.max(a,b,c))
+//     return Math.max(a,b,c)
+
+// }
+
+// Lrg(5,8,4)
+
+
+// function isEven(a) {
+//     if (a%2 == 0){
+//     console.log("Even")
+		
+//     }else{
+// 		console.log("Odd")
+// }
+// }
+// isEven(9)
+
+// Write a function that accepts a string as a parameter. 
+// If the length of the string is less than or equal to twenty characters long, return the string concatenated with itself (string + string). 
+// If the string is more than twenty characters long, return the first half of the string.
+
+ var txt = function(val){
+    if(val.length<=20){
+    console.log(val + val)
+    }else (val>val.length)
+    console.log(val.slice-val.length)
+    }
+
+    txt("ali")

cc65856c1bed1e928224d0f692ca8f0ee7417848
jalali2029 <learn@vschool.io>
2018-03-11 11:36:54 -0600

    Completing Array method

--- exercises/Array Methods Exercise/ArrayMethodExercise.js
+++ exercises/Array Methods Exercise/ArrayMethodExercise.js
@@ -1,35 +1,57 @@
 var fruit = ["banana", "apple", "orange", "watermelon"];
-
 var vegetables = ["carrot", "tomato", "pepper", "lettuce"];
- var removeLastFruit=fruit.pop()
-var removeLastVegetables=vegetables.pop()
-console.log("fruit:",fruit)
-console.log("vegetables:", vegetables)
 
-var addFirstFruit=fruit.unshift('Peach')
-var addFirstVegetables=vegetables.unshift('Potato')
-console.log("fruit:",fruit)
-console.log("vegetables:", vegetables)
-
-var indexofOrange=fruit.indexOf('orange')
-console.log("fruit:",fruit)
-
-var addnumFruit=fruit.push(3)
+console.log("1-Removing the last item from the vegetable array");
+var removeLastVegetables = vegetables.pop()
 console.log("fruit:", fruit)
-
-var lengthofvege=vegetables.length
-console.log("fruit:", fruit)
-
-var addnumVege=vegetables.push(4)
 console.log("vegetables:", vegetables)
+consol.log("")
 
-var food=fruit.concat(vegetables)
-console.log("food:", food)
+console.log("2-Removing the first item from the fruit array.");
+fruit.shift();
+console.log("fruit: ", fruit);
+console.log("vegetables: ", vegetables);
+console.log("")
 
-var removeElements=food.splice(4,2)
-// console.log(removeElements)
-console.log("food:", food)
+console.log("3-Index of Orange")
+var indexofOrange = fruit.indexOf('orange')
+console.log("fruit:", indexofOrange)
+console.log("")
 
-var rev=food.reverse()
-console.log("rev:", rev)
+console.log("4-Adding that number to the end of the fruit array");
+fruit.push(fruit.indexOf("orange"));
+console.log("fruit: ", fruit);
+console.log("vegetables: ", vegetables);
+console.log("")
 
+console.log("5-Use the length property to find the length of the vegetable array. Log that to the console.");
+console.log(vegetables.length);
+console.log("fruit: ", fruit);
+console.log("vegetables: ", vegetables);
+console.log("")
+
+
+console.log("6-Adding that Number to the end of vegetable array")
+vegetables.push(vegetables.length)
+console.log("fruit: ", fruit);
+console.log("vegetables: ", vegetables);
+console.log(" ");
+
+var food = fruit.concat(vegetables);
+console.log("7-Put the two arrays together into one array. Fruit first. Call the new Array 'food'.");
+console.log("food: ", food);
+console.log(" ");
+
+food.splice(4, 2);
+console.log("8-Remove 2 elements from your new array starting at index 4 with one method.");
+console.log("food: ", food);
+console.log(" ");
+
+food.reverse();
+console.log("9-Reverse your array.");
+console.log("food: ", food);
+console.log(" ");
+
+console.log("10-Log your array as a string to the console.");
+console.log(food.join());
+console.log(" ");
